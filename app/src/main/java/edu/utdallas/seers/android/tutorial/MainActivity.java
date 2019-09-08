680f83f7ae182455c7e109b4ae39d0a5680664df
juan <jmflorezff@gmail.com>
2019-09-08 17:14:46 -0500

    Main activity now displays user input
    - Added editText to main activity layout
    - Added onKey listener to main activity

--- app/src/main/java/edu/utdallas/seers/android/tutorial/MainActivity.java
+++ app/src/main/java/edu/utdallas/seers/android/tutorial/MainActivity.java
@@ -1,13 +1,37 @@
 package edu.utdallas.seers.android.tutorial;
 
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnKeyListener {
+
+    private EditText editText;
+    private TextView textView;
+
+    @Override
+    public boolean onKey(View view, int keyCode, KeyEvent keyEvent) {
+        if (keyEvent.getAction() == KeyEvent.ACTION_DOWN &&
+                keyCode == KeyEvent.KEYCODE_ENTER) {
+            textView.setText(editText.getText());
+            editText.setText("");
+
+            return true;
+        }
+        return false;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText_main_input);
+        textView = findViewById(R.id.textView_main_message);
+
+        editText.setOnKeyListener(this);
     }
 }
